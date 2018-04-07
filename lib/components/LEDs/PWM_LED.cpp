82c6adf03b92afde797e744d497e18c049f4d111
AndrewBiscuit <andrewalford1@outlook.com>
2018-04-07 13:17:28 +0100

    Added in my components

---
+++ lib/components/LEDs/PWM_LED.cpp
@@ -0,0 +1,27 @@
+#include <mbed.h>
+#include "PWM_LED.h"
+
+PWM_LED::PWM_LED(PinName pinName) : pwmOut(pinName)
+{
+    PWM_LED::pinName = pinName;
+}
+
+PinName PWM_LED::getPinName()
+{
+    return pinName;
+}
+
+void PWM_LED::on()
+{
+    pwmOut.write(0);
+}
+
+void PWM_LED::on(float brightness)
+{
+    pwmOut.write(brightness);
+}
+
+void PWM_LED::off()
+{
+    pwmOut.write(1);
+}
